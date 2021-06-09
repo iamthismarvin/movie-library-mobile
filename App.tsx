3f2c7b055a528f41ca891f04cf472e90565190d0
Marvin Parada <hello@marvin.dev>
2021-06-08 17:40:59 -0700

    NEW: Install [react-navigation].

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import Search from './screens/Search';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <Search />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Search" component={Search} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
