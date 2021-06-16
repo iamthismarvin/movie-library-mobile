181858deecb91f832df73a8ef0c45aa5e33ecc7e
Marvin Parada <hello@marvin.dev>
2021-06-15 20:22:12 -0700

    NEW: Add conditional [isDisabled] option to [CheckboxWithText] component.

--- components/CheckboxWithText.tsx
+++ components/CheckboxWithText.tsx
@@ -2,22 +2,46 @@ import React from 'react';
 import CheckBox from '@react-native-community/checkbox';
 import {StyleSheet, Text, View} from 'react-native';
 
-interface CheckboxWithTextProps {
+interface CheckboxWithTextProps extends InnerCheckboxProps {
   title: string;
-  value: boolean;
-  setFunction: Function;
 }
+
+interface InnerCheckboxProps {
+  value: boolean;
+  setFunction?: Function;
+  isDisabled?: boolean;
+}
+
+const InnerCheckbox = ({
+  value,
+  setFunction,
+  isDisabled,
+}: InnerCheckboxProps) => {
+  if (setFunction) {
+    return (
+      <CheckBox
+        value={value}
+        onValueChange={newValue => setFunction(newValue)}
+        disabled={isDisabled}
+      />
+    );
+  } else {
+    return <CheckBox value={value} disabled={isDisabled} />;
+  }
+};
 
 const CheckboxWithText = ({
   title,
   value,
   setFunction,
+  isDisabled,
 }: CheckboxWithTextProps) => {
   return (
     <View style={styles.container}>
-      <CheckBox
+      <InnerCheckbox
         value={value}
-        onValueChange={newValue => setFunction(newValue)}
+        setFunction={setFunction}
+        isDisabled={isDisabled}
       />
       <Text>{title}</Text>
     </View>
